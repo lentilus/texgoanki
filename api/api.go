d3eaa7117e7dfff1aeea6c2127ecaa71101d334b
lentilus <lentilus@mailbox.org>
2024-06-13 03:02:12 +0200

    feat: api: store media file

--- api/api.go
+++ api/api.go
@@ -39,8 +39,7 @@ func (a *AnkiConnect) Request(action string, params P, response R) error {
 		return err
 	}
 
-	fmt.Println(body)
-
+	// fmt.Println(bytes.NewBuffer(b))
 	req, err := http.NewRequest(http.MethodPost, a.Url, bytes.NewBuffer(b))
 	if err != nil {
 		return err
@@ -112,8 +111,97 @@ func CreateDeck(api API, name string) (int, error) {
 	return res.Result, nil
 }
 
-// func StoreMediaFile(api API, name sting, path string) (string, error) {}
-// func GetModels(api API,) ([]string, error) {}
-// func CreateModel(api API, name string, fields, ...) ([]string, error) {}
+// Stores file with base 64 encoded content.
+// for some reason the api is unable to process
+// absolute paths (the documentation suggests otherwise)
+func StoreMediaFile(api API, name string, data string) (string, error) {
+	type Response struct {
+		Result string // deck id
+		Error  any
+	}
+
+	res := new(Response)
+	params := map[string]any{"filename": name, "data": data}
+	err := api.Request("storeMediaFile", params, &res)
+	if err != nil {
+		return "", err
+	}
+
+	if res.Error != nil {
+		msg, ok := res.Error.(string)
+		if ok {
+			return "", errors.New(msg)
+		}
+		return "", errors.New("Cannot read error message.")
+	}
+
+	fmt.Println(res.Error)
+	return res.Result, nil
+}
+
+func GetModels(api API) ([]string, error) {
+	type Response struct {
+		Result []string // deck names
+		Error  any
+	}
+
+	res := new(Response)
+	err := api.Request("modelNames", nil, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.Error != nil {
+		msg, ok := res.Error.(string)
+		if ok {
+			return nil, errors.New(msg)
+		}
+		return nil, errors.New("Cannot read error message.")
+	}
+
+	return res.Result, nil
+}
+
+func CreateModel(
+	api API,
+	name string,
+	fields []string,
+	templates []map[string]string,
+) (any, error) {
+	params := map[string]any{
+		"modelName":     name,
+		"inOrderFields": fields,
+		"isCloze":       false,
+		"cardTemplates": templates,
+	}
+
+	type Response struct {
+		Result any // deck id
+		Error  any
+	}
+
+	res := new(Response)
+	err := api.Request("createModel", params, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.Error != nil {
+		msg, ok := res.Error.(string)
+		if ok {
+			return "", errors.New(msg)
+		}
+		return nil, errors.New("Cannot read error message.")
+	}
+
+	fmt.Println(res.Error)
+	return res.Result, nil
+
+}
+
+// This calls AddNote internally
+// func AddCard(api API, name string) (string, error) {
+// }
+
+// update Note (update note fields)
 // func Sync(api API) (error) {}
-// func AddCard(api API, name(string, error) {}
